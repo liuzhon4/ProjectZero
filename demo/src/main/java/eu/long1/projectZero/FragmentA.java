eb6f4ed5bd1deb10cf51d7cce3622a1cc18443a6
liuzhon4 <jmtc_nz@hotmail.com>
2018-01-05 16:35:06 +0800

    first commit

---
+++ demo/src/main/java/eu/long1/projectZero/FragmentA.java
@@ -0,0 +1,36 @@
+package eu.long1.projectZero;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FragmentA extends Fragment {
+    private View view;
+    private Button mButton;
+    private EditText mLast16Text;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_a, container, false);
+        mButton = (Button) view.findViewById(R.id.searchButton);
+        mLast16Text = (EditText) view.findViewById(R.id.last16View);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), mLast16Text.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+
+
+
+    }
+
+
+}
---
+++ demo/src/main/java/eu/long1/projectZero/MainActivity.java
@@ -0,0 +1,57 @@
+package eu.long1.projectZero;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    EditText txtBadgeNum, txtPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        txtBadgeNum = (EditText) findViewById(R.id.badge_num);
+        txtPassword = (EditText) findViewById(R.id.input_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtBadgeNum.getText().toString().trim().isEmpty()){
+                    txtBadgeNum.setError("请输入工号");
+                }else if(txtPassword.getText().toString().trim().isEmpty()){
+                    txtPassword.setError("请输入密码");
+                }else if (!authentication(txtBadgeNum.getText().toString().trim(), txtPassword.getText().toString().trim())) {
+                    Snackbar.make(v, "密码不正确，请联系管理员", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                } else {
+//                    Snackbar.make(v, "No validation errors, continue login", Snackbar.LENGTH_LONG).setAction("Action",null).show();
+                    Intent i = new Intent(getApplicationContext(), FrontActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+    }
+
+    public final static boolean isValidEmail(CharSequence target){
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public boolean authentication(String badgeNum, String pwd) {
+        return badgeNum.equals("364052") && pwd.equals("1");
+    }
+}
